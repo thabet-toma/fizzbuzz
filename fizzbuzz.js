e3af802af8ac335be829a296ed0ff7e188ef7c61
thabet toma <thapet64@gmail.com>
2022-08-29 12:52:58 +0300

    fizzbuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,15 @@
+for(var i=0;i<=100;i++){
+    if(i%3==0&& i%5!=0){
+        console.log("fizz");
+    }
+     if(i%5==0&&i%3!=0){
+        console.log("buzz")
+    }
+    if(i%3==0&&i%5==0){
+        console.log("fizzbuzz")
+    }
+    else{
+        console.log(i);
+    }
+
+}
